e997123a6927506599c3378019760b8fced599f8
DanShinde <112338485+DanShinde@users.noreply.github.com>
2025-01-13 14:39:24 +0530

    Added logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import RegisterView, LoginView,ProfileView
+from .views import RegisterView, LoginView,ProfileView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('profile/', ProfileView.as_view()),
+    path('logout/', LogoutView.as_view(), name ='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,18 @@ class LoginView(APIView):
             })
         return Response({"message": "Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
-
+class LogoutView(APIView):
+     permission_classes = [IsAuthenticated]
+     def post(self, request):
+          
+          try:
+               refresh_token = request.data["refresh_token"]
+               token = RefreshToken(refresh_token)
+               token.blacklist()
+               return Response(status=status.HTTP_205_RESET_CONTENT)
+          except Exception as e:
+               return Response(status=status.HTTP_400_BAD_REQUEST)
+          
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
