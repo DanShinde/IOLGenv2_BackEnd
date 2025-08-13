5bd9a754095154c873c9962675e91c9ebe722c08
danshinde <danshinde@github.com>
2025-08-13 18:08:26 +0530

    added cache clearing url

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import home, downloads
+from .views import home, downloads, clear_cache
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('downloads/', downloads, name='downloads'),
+    path('cacheclear/', clear_cache, name='cache-clear'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from accounts.models import Info
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 
 
 @login_required
@@ -16,3 +18,19 @@ def downloads(request):
         'infos': infos
     }
     return render(request, 'home/downloads.html', context)
+
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.core.exceptions import PermissionDenied
+
+def superuser_required(view_func):
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_superuser:
+            raise PermissionDenied
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+@superuser_required
+def clear_cache(request):
+    cache.clear()
+    return JsonResponse({"status": "ok", "message": "Cache cleared"})
